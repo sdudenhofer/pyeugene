ce1c7bb720cf6b751e044c8957c51c8c2b608724
sdudenhofer <sdudenhofer@gmail.com>
2020-04-05 18:44:26 -0700

    add 404 page logic'

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 def hello_world():
     return render_template("index.html")
 
-    
+@app.errorhandler(404)
+def page_not_found(error):
+   return render_template('404.html', title = '404'), 404
+       
 if __name__ == "__main__":
     app.run(debug=True)
